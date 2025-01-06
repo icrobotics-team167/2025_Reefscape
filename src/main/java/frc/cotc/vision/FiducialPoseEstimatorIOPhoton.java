11eb3cb616bf0cf1fa046ca28af4e61ec045fb79
ハイドラント <thecomputer314@gmail.com>
2025-01-05 19:39:42 -0600

    Add AKit and comment out Choreo/Photon code
    - Tada

--- src/main/java/frc/cotc/vision/FiducialPoseEstimatorIOPhoton.java
+++ src/main/java/frc/cotc/vision/FiducialPoseEstimatorIOPhoton.java
@@ -9,17 +9,9 @@ package frc.cotc.vision;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.util.Units;
-import frc.cotc.Robot;
-import java.util.ArrayList;
-import org.photonvision.PhotonCamera;
-import org.photonvision.PhotonPoseEstimator;
-import org.photonvision.simulation.PhotonCameraSim;
-import org.photonvision.simulation.SimCameraProperties;
-import org.photonvision.simulation.VisionSystemSim;
 
 public class FiducialPoseEstimatorIOPhoton implements FiducialPoseEstimatorIO {
   private static final AprilTagFieldLayout tags;
@@ -28,8 +20,8 @@ public class FiducialPoseEstimatorIOPhoton implements FiducialPoseEstimatorIO {
     tags = AprilTagFieldLayout.loadField(AprilTagFields.k2024Crescendo);
   }
 
-  private final PhotonCamera camera;
-  private final PhotonPoseEstimator poseEstimator;
+  //  private final PhotonCamera camera;
+  //  private final PhotonPoseEstimator poseEstimator;
   private final Transform3d cameraPos;
 
   public FiducialPoseEstimatorIOPhoton(int id) {
@@ -74,51 +66,53 @@ public class FiducialPoseEstimatorIOPhoton implements FiducialPoseEstimatorIO {
       default -> throw new IndexOutOfBoundsException();
     }
 
-    camera = new PhotonCamera(name);
-    poseEstimator =
-        new PhotonPoseEstimator(
-            tags, PhotonPoseEstimator.PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, cameraPos);
-
-    if (Robot.isSimulation()) {
-      var cameraProps = new SimCameraProperties();
-      cameraProps.setCalibError(.25, .1);
-      cameraProps.setAvgLatencyMs(5);
-      cameraProps.setLatencyStdDevMs(2);
-      cameraProps.setExposureTimeMs(5);
-      cameraProps.setFPS(40);
-      VisionSim.getInstance().addSimCamera(new PhotonCameraSim(camera, cameraProps), cameraPos);
-    }
+    //    camera = new PhotonCamera(name);
+    //    poseEstimator =
+    //        new PhotonPoseEstimator(
+    //            tags, PhotonPoseEstimator.PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, cameraPos);
+    //
+    //    if (Robot.isSimulation()) {
+    //      var cameraProps = new SimCameraProperties();
+    //      cameraProps.setCalibError(.25, .1);
+    //      cameraProps.setAvgLatencyMs(5);
+    //      cameraProps.setLatencyStdDevMs(2);
+    //      cameraProps.setExposureTimeMs(5);
+    //      cameraProps.setFPS(40);
+    //      VisionSim.getInstance().addSimCamera(new PhotonCameraSim(camera, cameraProps),
+    // cameraPos);
+    //    }
   }
 
   @Override
   public void updateInputs(FiducialPoseEstimatorIOInputs inputs) {
     inputs.hasNewData = false;
 
-    var results = camera.getAllUnreadResults();
-    if (results.isEmpty()) {
-      return;
-    }
+    //    var results = camera.getAllUnreadResults();
+    //    if (results.isEmpty()) {
+    //      return;
+    //    }
 
-    var estimateList = new ArrayList<PoseEstimate>();
-    for (var result : results) {
-      poseEstimator
-          .update(result)
-          .ifPresent(
-              (estimate) -> {
-                inputs.hasNewData = true;
+    //    var estimateList = new ArrayList<PoseEstimate>();
+    //    for (var result : results) {
+    //      poseEstimator
+    //          .update(result)
+    //          .ifPresent(
+    //              (estimate) -> {
+    //                inputs.hasNewData = true;
+    //
+    //                var tagAbsolutePoses = new Pose3d[estimate.targetsUsed.size()];
+    //                for (int i = 0; i < tagAbsolutePoses.length; i++) {
+    //                  var tagRelativePos = estimate.targetsUsed.get(i).getBestCameraToTarget();
+    //                  tagAbsolutePoses[i] =
+    // estimate.estimatedPose.plus(cameraPos.plus(tagRelativePos));
+    //                }
+    //                estimateList.add(
+    //                    new PoseEstimate(
+    //                        estimate.estimatedPose, estimate.timestampSeconds, tagAbsolutePoses));
+    //              });
+    //    }
 
-                var tagAbsolutePoses = new Pose3d[estimate.targetsUsed.size()];
-                for (int i = 0; i < tagAbsolutePoses.length; i++) {
-                  var tagRelativePos = estimate.targetsUsed.get(i).getBestCameraToTarget();
-                  tagAbsolutePoses[i] = estimate.estimatedPose.plus(cameraPos.plus(tagRelativePos));
-                }
-                estimateList.add(
-                    new PoseEstimate(
-                        estimate.estimatedPose, estimate.timestampSeconds, tagAbsolutePoses));
-              });
-    }
-
-    inputs.poseEstimates = estimateList.toArray(new PoseEstimate[0]);
+    //    inputs.poseEstimates = estimateList.toArray(new PoseEstimate[0]);
   }
 
   public static class VisionSim {
@@ -131,21 +125,21 @@ public class FiducialPoseEstimatorIOPhoton implements FiducialPoseEstimatorIO {
       return visionSim;
     }
 
-    final VisionSystemSim systemSim;
-
-    public VisionSim() {
-      systemSim = new VisionSystemSim("main");
-      systemSim.addAprilTags(tags);
-    }
-
-    public void addSimCamera(PhotonCameraSim cameraSim, Transform3d cameraPosition) {
-      cameraSim.enableProcessedStream(false);
-      cameraSim.enableRawStream(false);
-      systemSim.addCamera(cameraSim, cameraPosition);
-    }
+    //    final VisionSystemSim systemSim;
+    //
+    //    public VisionSim() {
+    //      systemSim = new VisionSystemSim("main");
+    //      systemSim.addAprilTags(tags);
+    //    }
+    //
+    //    public void addSimCamera(PhotonCameraSim cameraSim, Transform3d cameraPosition) {
+    //      cameraSim.enableProcessedStream(false);
+    //      cameraSim.enableRawStream(false);
+    //      systemSim.addCamera(cameraSim, cameraPosition);
+    //    }
 
     public void update() {
-      systemSim.update(Robot.groundTruthPoseSupplier.get());
+      //      systemSim.update(Robot.groundTruthPoseSupplier.get());
     }
   }
 }
