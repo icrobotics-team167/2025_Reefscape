2aaf724fa26866876f7b7a692b988875baecd5f0
ハイドラント <thecomputer314@gmail.com>
2025-03-06 15:04:37 -0600

    Pull back the numbers to not overshoot
    - Tada

--- src/main/java/frc/cotc/superstructure/AlgaePivot.java
+++ src/main/java/frc/cotc/superstructure/AlgaePivot.java
@@ -37,7 +37,7 @@ class AlgaePivot extends SubsystemBase {
   }
 
   Command barge() {
-    return run(() -> setTargetPos(Units.degreesToRadians(115))).withName("Barge");
+    return run(() -> setTargetPos(Units.degreesToRadians(110))).withName("Barge");
   }
 
   Command stow() {
