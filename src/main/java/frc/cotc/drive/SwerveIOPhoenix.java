b590e78370157afad52cce4bef305a1f4de4b2e5
ハイドラント <thecomputer314@gmail.com>
2025-03-08 10:51:08 -0600

    Roll back current limit a little bit

--- src/main/java/frc/cotc/drive/SwerveIOPhoenix.java
+++ src/main/java/frc/cotc/drive/SwerveIOPhoenix.java
@@ -97,7 +97,7 @@ public class SwerveIOPhoenix implements SwerveIO {
                             * ((CONSTANTS.MASS_KG / 4) * 9.81)
                             * CONSTANTS.WHEEL_DIAMETER_METERS
                             / 2)),
-            120);
+            90);
   }
 
   private final Module[] modules = new Module[4];
