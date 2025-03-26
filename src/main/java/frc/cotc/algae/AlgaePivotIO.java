e3e8182550853e8b4414e4f010b8ca2fd08fa3b1
ハイドラント <thecomputer314@gmail.com>
2025-03-26 10:05:37 -0500

    Move algae stuff to their own package
    - Tada

--- src/main/java/frc/cotc/algae/AlgaePivotIO.java
+++ src/main/java/frc/cotc/algae/AlgaePivotIO.java
@@ -5,7 +5,7 @@
 // license that can be found in the LICENSE file at
 // the root directory of this project.
 
-package frc.cotc.superstructure;
+package frc.cotc.algae;
 
 import frc.cotc.util.MotorCurrentDraws;
 import org.littletonrobotics.junction.LogTable;
