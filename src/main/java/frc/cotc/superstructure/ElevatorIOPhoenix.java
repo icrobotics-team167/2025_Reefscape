65c82cbadbb2f7c79e6726ac4bd9bad75adac51d
ハイドラント <thecomputer314@gmail.com>
2025-04-13 15:34:34 -0500

    New kG
    - Tada

--- src/main/java/frc/cotc/superstructure/ElevatorIOPhoenix.java
+++ src/main/java/frc/cotc/superstructure/ElevatorIOPhoenix.java
@@ -76,7 +76,7 @@ public class ElevatorIOPhoenix implements ElevatorIO {
     config.SoftwareLimitSwitch.ReverseSoftLimitThreshold = 0;
     config.SoftwareLimitSwitch.ReverseSoftLimitEnable = true;
     config.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
-    config.Slot0.kG = .2;
+    config.Slot0.kG = .405;
     config.Slot0.kS = Robot.isReal() ? .03 : 0;
     config.Slot0.kV = 12.0 / ((5800 / 60.0) / gearRatio);
     config.Slot0.kA = config.Slot0.kG / 9.81 * metersPerRotation;
@@ -91,7 +91,7 @@ public class ElevatorIOPhoenix implements ElevatorIO {
             .001);
     config.Slot0.kP = firstStageGains.kP();
     config.Slot0.kD = firstStageGains.kD();
-    config.Slot1.kG = .28;
+    config.Slot1.kG = .496;
     config.Slot1.kS = Robot.isReal() ? .03 : 0;
     config.Slot1.kV = config.Slot0.kV;
     config.Slot1.kA = config.Slot1.kG / 9.81 * metersPerRotation;
