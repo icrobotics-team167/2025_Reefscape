c8e92d48c1dda354bd973c3f0923ecee6a723e16
ハイドラント <thecomputer314@gmail.com>
2025-04-14 15:57:29 -0500

    Don't rumble controller in auto
    - Tada

--- src/main/java/frc/cotc/Robot.java
+++ src/main/java/frc/cotc/Robot.java
@@ -252,8 +252,10 @@ public class Robot extends LoggedRobot {
     superstructure.coralStuck().debounce(.25).onTrue(superstructure.ejectStuckCoral());
 
     new Trigger(superstructure::hasCoral)
+Do        .and(RobotModeTriggers.teleop())
         .onChange(secondary.rumble(.35).withName("Rumble secondary controller"));
     new Trigger(superstructure::hasAlgae)
+        .and(RobotModeTriggers.teleop())
         .onChange(secondary.rumble(.35).withName("Rumble secondary controller"));
 
     autos = new Autos(swerve, superstructure);
